2f1f79a8e53693be35a596a105dd265f8e59d1d5
井上陽介 <sukebanchou@icloud.com>
2025-05-04 20:25:26 +0900

    ファビコン設定

--- resources/views/components/layouts/head.blade.php
+++ resources/views/components/layouts/head.blade.php
@@ -2,6 +2,7 @@
     <title>MGJ2025</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('images/favicon.ico?v='.config('app.version')) }}">
     <link rel="stylesheet" href="{{ asset('css/app.css?v='.config('app.version')) }}">
     @stack('styles')
 </head>
